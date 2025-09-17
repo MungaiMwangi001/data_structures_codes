d20222d180c36e7d565bf7220e4d33375b105fce
Stephen Mungai <mungaistephen066@gmail.com>
2025-09-17 19:06:19 +0300

    647

---
+++ Leetcode_questions/easy/647palindromic_sbstring.py
@@ -0,0 +1,40 @@
+'''Given a string s, return the number of palindromic substrings in it.
+
+A string is a palindrome when it reads the same backward as forward.
+
+A substring is a contiguous sequence of characters within the string.
+
+ 
+
+Example 1:
+
+Input: s = "abc"
+Output: 3
+Explanation: Three palindromic strings: "a", "b", "c".
+Example 2:
+
+Input: s = "aaa"
+Output: 6
+Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+ '''
+
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        n = len(s)
+        count = 0
+
+        # Function to expand around center and count palindromes
+        def expand_around_center(left: int, right: int) -> int:
+            nonlocal count
+            while left >= 0 and right < n and s[left] == s[right]:
+                count += 1
+                left -= 1
+                right += 1
+
+        for i in range(n):
+            # Odd length palindromes
+            expand_around_center(i, i)
+            # Even length palindromes
+            expand_around_center(i, i + 1)
+
+        return count
